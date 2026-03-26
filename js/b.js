e434c1c6292f3b1f7b48c2b5c5f33c5de2fc6baa
Lia <h.mountain.phillips@gmail.com>
2026-03-26 11:19:14 +0000

    pruebitas

--- js/b.js
+++ js/b.js
@@ -1,3 +1,7 @@
 function añadirTarea() {
-    
+    const inputTarea = document.getElementById("inputTarea")
+    const salidaTarea = document.getElementById("tareas")
+    if (inputTarea != "") {
+        salidaTarea.innerHTML = "<div>" + inputTarea.value + "<button>X</button></div>"
+    }
 }
